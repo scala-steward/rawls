3d79c4554f3eae6044a03a86f3eb3ffa1e5cd425
Rob Title <rtitle@broadinstitute.org>
2017-07-13 13:52:16 -0400

    Experimental: use truncate instead of delete for cleaning up test data for better performance

--- core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/slick/DataAccess.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/dataaccess/slick/DataAccess.scala
@@ -1,7 +1,12 @@
 package org.broadinstitute.dsde.rawls.dataaccess.slick
 
 import org.broadinstitute.dsde.rawls.expressions.SlickExpressionParser
+import slick.dbio.Effect.Read
 import slick.driver.JdbcProfile
+import slick.jdbc.SQLActionBuilder
+import slick.jdbc.SetParameter.SetUnit
+import slick.jdbc.meta.MTable
+import slick.profile.SqlAction
 
 trait DataAccess
   extends PendingBucketDeletionComponent
@@ -25,42 +30,31 @@ trait DataAccess
   
   import driver.api._
 
-  def truncateAll: WriteAction[Int] = {
-    // important to keep the right order for referential integrity !
-    // if table X has a Foreign Key to table Y, delete table X first
+  def safeTableNames: DBIOAction[Seq[String], NoStream, Effect.Read] = {
+    MTable.getTables.map { tables =>
+      tables.map(_.name.name) flatMap {
+        case "GROUP" => Option("`GROUP`")
+        case "USER" => Option("`USER`")
+        case "DATABASECHANGELOG" => None // managed by Liquibase
+        case "DATABASECHANGELOGLOCK" => None // managed by Liquibase
+        case other => Option(other)
+      }
+    }
+  }
 
-    TableQuery[GroupSubgroupsTable].delete andThen                // FK to group
-      TableQuery[GroupUsersTable].delete andThen                  // FK to group, users
-      TableQuery[WorkspaceAccessTable].delete andThen             // FK to group, workspace
-      TableQuery[RawlsBillingProjectGroupTable].delete andThen    // FK to group, billingproject
-      TableQuery[EntityAttributeTable].delete andThen             // FK to entity
-      TableQuery[WorkspaceAttributeTable].delete andThen          // FK to entity, workspace
-      TableQuery[SubmissionAttributeTable].delete andThen         // FK to entity, submissionvalidation
-      TableQuery[MethodConfigurationInputTable].delete andThen    // FK to MC
-      TableQuery[MethodConfigurationOutputTable].delete andThen   // FK to MC
-      TableQuery[MethodConfigurationPrereqTable].delete andThen   // FK to MC
-      TableQuery[SubmissionValidationTable].delete andThen        // FK to workflow, workflowfailure
-      TableQuery[WorkflowMessageTable].delete andThen             // FK to workflow
-      TableQuery[WorkflowTable].delete andThen                    // FK to submission, entity
-      TableQuery[SubmissionTable].delete andThen                  // FK to workspace, user, MC, entity
-      TableQuery[MethodConfigurationTable].delete andThen         // FK to workspace
-      TableQuery[EntityTable].delete andThen                      // FK to workspace
-      TableQuery[PendingWorkspaceAccessTable].delete andThen      // FK to workspace, user
-      TableQuery[WorkspaceUserShareTable].delete andThen          // FK to workspace, user
-      TableQuery[WorkspaceGroupShareTable].delete andThen         // FK to workspace, group
-      TableQuery[WorkspaceUserCatalogTable].delete andThen        // FK to workspace, user
-      TableQuery[WorkspaceGroupCatalogTable].delete andThen       // FK to workspace, group
-      TableQuery[WorkspaceTable].delete andThen                   // FK to realm
-      TableQuery[ManagedGroupTable].delete andThen                       // FK to group
-      TableQuery[RawlsBillingProjectTable].delete andThen
-      TableQuery[RawlsGroupTable].delete andThen
-      TableQuery[RawlsUserTable].delete andThen
-      TableQuery[WorkflowAuditStatusTable].delete andThen
-      TableQuery[SubmissionAuditStatusTable].delete andThen
-      TableQuery[PendingBucketDeletionTable].delete andThen
-      TableQuery[EntityAttributeScratchTable].delete andThen
-      TableQuery[WorkspaceAttributeScratchTable].delete andThen
-      TableQuery[ExprEvalScratch].delete
+  def truncateAll: DBIOAction[Int, NoStream, Effect.Read with Effect] = {
+    val truncates = safeTableNames flatMap { tableNames =>
+      DBIO.fold(
+        tableNames.map(tableName => SQLActionBuilder(List(s"TRUNCATE TABLE $tableName"), SetUnit).asUpdate), 0)(_ + _)
+    }
+
+    // need to temporarily disable foreign key checks for the truncate commands
+    DBIO.sequence(
+      Seq(
+        sqlu"""SET FOREIGN_KEY_CHECKS = 0;""",
+        truncates,
+        sqlu"""SET FOREIGN_KEY_CHECKS = 1;"""
+      )).map(results => results(1))  // return the results of the truncate queries
   }
 
   def sqlDBStatus() = {
--- core/src/test/scala/org/broadinstitute/dsde/rawls/dataaccess/slick/DataAccessSpec.scala
+++ core/src/test/scala/org/broadinstitute/dsde/rawls/dataaccess/slick/DataAccessSpec.scala
@@ -1,7 +1,5 @@
 package org.broadinstitute.dsde.rawls.dataaccess.slick
 
-import slick.jdbc.meta.MTable
-
 /**
  * Created by thibault on 6/1/16.
  */
@@ -10,17 +8,9 @@ class DataAccessSpec extends TestDriverComponentWithFlatSpecAndMatchers {
   import driver.api._
 
   "DataAccess" should "test that truncateAll has left the DB in a known empty state" in withEmptyTestDatabase {
-    val rawTableNames: Seq[String] = runAndWait(MTable.getTables).map(_.name.name)
+    val tables = runAndWait(safeTableNames)
 
-    val safeTableNames: Seq[String] = rawTableNames flatMap {
-      case "GROUP" => Option("`GROUP`")
-      case "USER" => Option("`USER`")
-      case "DATABASECHANGELOG" => None        // managed by Liquibase
-      case "DATABASECHANGELOGLOCK" => None    // managed by Liquibase
-      case other => Option(other)
-    }
-
-    safeTableNames foreach { tableName =>
+    tables foreach { tableName =>
       val count = sql"SELECT COUNT(*) FROM #$tableName "
       assertResult(0, tableName + " not empty") {
         runAndWait(count.as[Int].head)
