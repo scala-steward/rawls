6ef10a3abe37b29b1b43f1b47b39977610f3f9f3
Rob Title <rtitle@broadinstitute.org>
2017-08-01 05:06:26 -0400

    Stackdriver to grafana

--- core/src/main/scala/org/broadinstitute/dsde/rawls/Boot.scala
+++ core/src/main/scala/org/broadinstitute/dsde/rawls/Boot.scala
@@ -79,7 +79,8 @@ object Boot extends App with LazyLogging {
             startStatsDReporter(
               statsDConf.getString("host"),
               statsDConf.getInt("port"),
-              statsDConf.getDuration("period"))
+              statsDConf.getDuration("period"),
+              apiKey = statsDConf.getStringOption("apiKey"))
           case (other, _) =>
             logger.warn(s"Unknown metrics backend: $other")
         }
@@ -277,9 +278,10 @@ object Boot extends App with LazyLogging {
     }
   }
 
-  def startStatsDReporter(host: String, port: Int, period: java.time.Duration): Unit = {
+  def startStatsDReporter(host: String, port: Int, period: java.time.Duration, registryName: String = "default", apiKey: Option[String] = None): Unit = {
     logger.info(s"Starting statsd reporter writing to [$host:$port] with period [${period.toMillis} ms]")
-    val reporter = StatsDReporter.forRegistry(SharedMetricRegistries.getOrCreate("default"))
+    val reporter = StatsDReporter.forRegistry(SharedMetricRegistries.getOrCreate(registryName))
+      .prefixedWith(apiKey.orNull)
       .convertRatesTo(TimeUnit.SECONDS)
       .convertDurationsTo(TimeUnit.MILLISECONDS)
       .build(host, port)
